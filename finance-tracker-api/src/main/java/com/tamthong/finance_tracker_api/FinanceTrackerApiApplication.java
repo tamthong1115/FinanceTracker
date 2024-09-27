3cc98243f68daf462c353a721662a6d3a6131dfd
Tam Thong <120663808+tamthong1115@users.noreply.github.com>
2024-09-27 11:09:57 +0700

    Initial project
    docker
    
    create layout for frontend
    
    remove pgadmin

---
+++ finance-tracker-api/src/main/java/com/tamthong/finance_tracker_api/FinanceTrackerApiApplication.java
@@ -0,0 +1,13 @@
+package com.tamthong.finance_tracker_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceTrackerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceTrackerApiApplication.class, args);
+	}
+
+}
---
+++ finance-tracker-api/src/test/java/com/tamthong/finance_tracker_api/FinanceTrackerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tamthong.finance_tracker_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceTrackerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
