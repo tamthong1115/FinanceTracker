3bd56767f6cda1f772c0ded54dc37900968a193a
ndang <ndang2319@gmail.com>
2024-10-27 18:26:14 +0700

    fix error mapper of transaction

--- finance-tracker-api/src/main/java/com/tamthong/finance_tracker_api/mapper/TransactionMapper.java
+++ finance-tracker-api/src/main/java/com/tamthong/finance_tracker_api/mapper/TransactionMapper.java
@@ -2,10 +2,9 @@ package com.tamthong.finance_tracker_api.mapper;
 
 import com.tamthong.finance_tracker_api.dto.TransactionDTO;
 import com.tamthong.finance_tracker_api.model.Transaction;
-import com.tamthong.finance_tracker_api.model.User;
 import org.mapstruct.*;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface TransactionMapper {
     @Mapping(target = "userId", source = "user.id")
     TransactionDTO toDTO(Transaction transaction);
@@ -14,13 +13,4 @@ public interface TransactionMapper {
     @Mapping(target = "createdAt", ignore = true)
     @Mapping(target = "updatedAt", ignore = true)
     Transaction toEntity(TransactionDTO dto);
-
-    @AfterMapping
-    default void mapUserReference(@MappingTarget Transaction transaction, TransactionDTO dto) {
-        if (dto.getUserId() != null) {
-            User user = new User();
-            user.setId(dto.getUserId());
-            transaction.setUser(user);
-        }
-    }
 }
