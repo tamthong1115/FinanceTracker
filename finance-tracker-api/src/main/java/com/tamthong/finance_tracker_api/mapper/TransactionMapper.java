75a01cb8ad39a295baf93a68571a44bdbe2fe88b
ndang <ndang2319@gmail.com>
2024-10-27 19:56:11 +0700

    Add user reference mapping in TransactionMapper

--- finance-tracker-api/src/main/java/com/tamthong/finance_tracker_api/mapper/TransactionMapper.java
+++ finance-tracker-api/src/main/java/com/tamthong/finance_tracker_api/mapper/TransactionMapper.java
@@ -2,6 +2,7 @@ package com.tamthong.finance_tracker_api.mapper;
 
 import com.tamthong.finance_tracker_api.dto.TransactionDTO;
 import com.tamthong.finance_tracker_api.model.Transaction;
+import com.tamthong.finance_tracker_api.model.User;
 import org.mapstruct.*;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
@@ -13,4 +14,13 @@ public interface TransactionMapper {
     @Mapping(target = "createdAt", ignore = true)
     @Mapping(target = "updatedAt", ignore = true)
     Transaction toEntity(TransactionDTO dto);
+
+    @AfterMapping
+    default void mapUserReference(@MappingTarget Transaction transaction, TransactionDTO dto) {
+        if (dto.getUserId() != null) {
+            User user = new User();
+            user.setId(dto.getUserId());
+            transaction.setUser(user);
+        }
+    }
 }
